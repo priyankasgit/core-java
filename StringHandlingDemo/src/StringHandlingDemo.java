23ccc4ac51437c4dabf6484991ea11bf847c3929
Priyanka <Priyanka@LAPTOP-KEJRG52R>
2022-06-14 12:50:44 +0530

    first commit

---
+++ StringHandlingDemo/src/StringHandlingDemo.java
@@ -0,0 +1,31 @@
+
+public class StringHandlingDemo {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		String s1="Dr.AIT";
+		System.out.println("The length of s1 is:"+s1.length());
+		System.out.println("The index of T is:"+s1.indexOf('T'));
+		String s2="Monday";
+		String s3=s2.substring(3,6);
+		String s8=s2.substring(3);
+		System.out.println(s3);
+		System.out.println(s8);
+		String s4="   Hi   ";
+		String s5=s4.trim();
+		System.out.println(s5);
+		String sentence="Hi welcome to java class";
+		char ch[]=sentence.toCharArray();
+		for(int i=0;i<sentence.length();i++)
+		{
+			System.out.println(ch[i]);	
+		}
+		System.out.println(sentence.length());
+		String s6="java";
+		System.out.println(s6);
+		String s7=s6.toUpperCase();
+		System.out.println(s7);
+			}
+	}
+
